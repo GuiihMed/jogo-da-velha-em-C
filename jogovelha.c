6925036f0b7f98660cc1b835a8b3976a085fab19
renan-mr <renanmarinhodossantos9@gmail.com>
2024-11-27 16:15:29 -0300

    Update jogovelha.c

--- jogovelha.c
+++ jogovelha.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
-#include <stdlib.h> // Biblioteca p/ gerenciar a memória
-#include <time.h> // Biblioteca p/ aprimorar o rand
+#include <stdlib.h> // Biblioteca p/ gerenciar a memória  e uso do rand
+#include <time.h> // Biblioteca p/ usar como semente para o rand
 #include <locale.h> // Biblioteca p/ corrigir acentuação
 #include <string.h>
 
